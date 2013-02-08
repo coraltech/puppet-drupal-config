1e1d308015a11e93dfd021abb79f76b3d511c1cf
Adrian Webb <adrian.webb@coraltech.net>
2013-02-08 14:35:56 -0500

    Adding an environment.sh file for inclusion in base server images (important facts).

---
+++ environment.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# This file should be placed into /etc/profile.d (on Ubuntu servers)
+#
+# Modify these values accordingly for your needs
+#
+export FACTER_server_location='datacenter'
+export FACTER_server_environment='production'
